6578f9a21224f3d30e2a57d03d5cbd550768d005
Rainer Standke <rainer@xmil.biz>
2017-02-09 11:55:17 -0800

    Initial Commit

---
+++ PiVolumeSwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PiVolumeSwift
+//
+//  Created by Rainer Standke on 2/9/17.
+//  Copyright © 2017 Rainer Standke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ PiVolumeSwift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PiVolumeSwift
+//
+//  Created by Rainer Standke on 2/9/17.
+//  Copyright © 2017 Rainer Standke. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
