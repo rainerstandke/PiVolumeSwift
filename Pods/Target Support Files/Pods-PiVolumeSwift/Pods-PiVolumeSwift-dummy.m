b4ef192bdd77a9a3ac8a8dbaba3cc2fff9329367
Rainer Standke <rainer@xmil.biz>
2017-02-09 12:08:36 -0800

    initial commit

---
+++ Pods/Target Support Files/Pods-PiVolumeSwift/Pods-PiVolumeSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PiVolumeSwift : NSObject
+@end
+@implementation PodsDummy_Pods_PiVolumeSwift
+@end
---
+++ Pods/Target Support Files/Pods-PiVolumeSwiftTests/Pods-PiVolumeSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PiVolumeSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_PiVolumeSwiftTests
+@end
